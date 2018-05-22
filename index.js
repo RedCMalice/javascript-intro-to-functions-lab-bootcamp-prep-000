ef9837fd75fefd72b2d52a9ec96369d738b8d3bf
Chong Mi Land <cyun1979@gmail.com>
2018-05-22 01:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
+<<<<<<< HEAD
 }
 function whisper(string) {
   return string.toLowerCase()
+=======
+>>>>>>> 707747581c2ef9aa6ed10f2295850867a6063a26
 }
